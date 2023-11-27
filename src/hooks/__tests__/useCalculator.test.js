919119a2b15fa76fa9605c628798977d694db7c3
mayu <mayu.k0219sun@gmail.com>
2023-11-27 05:06:56 +0000

    テストコード修正

--- src/hooks/__tests__/useCalculator.test.js
+++ src/hooks/__tests__/useCalculator.test.js
@@ -1,2 +1,44 @@
 import { renderHook, act } from '@testing-library/react';
+import Value from '../../consts/Value';
 import useCalculator from '../useCalculator';
+
+describe('基本仕様', () => {
+    test('初期表示　resultValue:0', () => {
+        const {result} = renderHook(() => useCalculator());
+        expect(result.current.resultValue).toBe("0");
+    });
+    test('数字ボタンを押す', () => {
+        const {result} = renderHook(() => useCalculator());
+        act(() => result.current.handleNumberButtonClick(Value.NINE));
+        expect(result.current.resultValue).toBe(9);
+        act(() => result.current.handleNumberButtonClick(Value.NINE));
+        expect(result.current.resultValue).toBe(99);
+    });
+    // test('一桁の足し算をする', () => {
+    //     const {result} = renderHook(() => useCalculator());
+    //     act(() => result.current.handleNumberButtonClick("1"));
+    //     act(() => result.current.handleOperatorButtonClick("+"));
+    //     act(() => result.current.handleNumberButtonClick("1"));
+    //     act(() => result.current.handleOperatorButtonClick("="));
+    //     expect(result.current.resultValue).toBe(2);
+    // })
+    // test('二桁の足し算をする', () => {
+    //     const {result} = renderHook(() => useCalculator());
+    //     act(() => result.current.handleNumberButtonClick("1"));
+    //     act(() => result.current.handleNumberButtonClick("0"));
+    //     act(() => result.current.handleOperatorButtonClick("+"));
+    //     act(() => result.current.handleNumberButtonClick("2"));
+    //     act(() => result.current.handleNumberButtonClick("0"));
+    //     act(() => result.current.handleOperatorButtonClick("="));
+    //     expect(result.current.resultValue).toBe(30);
+    // })
+    // test('連続して二桁の足し算をする', () => {
+    //     const {result} = renderHook(() => useCalculator());
+    //     act(() => result.current.handleNumberButtonClick(1));
+    //     act(() => result.current.handleOperatorButtonClick("+"));
+    //     act(() => result.current.handleNumberButtonClick(15));
+    //     act(() => result.current.handleOperatorButtonClick("+"));
+    //     expect(result.current.resultValue).toBe(20);
+
+    // })
+});
