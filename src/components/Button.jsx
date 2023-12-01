480b68b72baf8438b16e349e17b1f745abe743cb
mayu <mayu.k0219sun@gmail.com>
2023-12-01 05:06:58 +0000

    Buttonスタイル追加

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,19 @@
 import React from 'react';
+import {isNumber, isOperator, isEqual, isClear } from '../utils/valueUtils';
 
 export function Button({ value, handle }){
+    let buttonClassName;
+
+    if(isOperator(value)){
+        buttonClassName = "operator-button";
+    }else if(isEqual(value)){
+        buttonClassName = "equal-button";
+    }else if(isClear(value)){
+        buttonClassName = "clear-button";
+    }else if(isNumber(value)){
+        buttonClassName = "number-button";
+    }
+
     return(
         <button onClick={() => handle(value)}>
             {value}
